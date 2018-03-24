b7afc4e7180cae936051fc09e73e2989322d4c64
Yuri Tret <yuri.tret@gmail.com>
2018-03-24 23:51:49 +0200

    assembly

--- clib/Properties/AssemblyInfo.cs
+++ clib/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("clib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("yuri.tret")]
 [assembly: AssemblyProduct("clib")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.28.0324")]
-[assembly: AssemblyFileVersion("1.0.28.0324")]
+[assembly: AssemblyVersion("1.0.29.0324")]
+[assembly: AssemblyFileVersion("1.0.29.0324")]
